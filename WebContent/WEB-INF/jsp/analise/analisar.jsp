b271b3797c48c2b8bf12e44a08feb4b7895f9cad
Renan de Andrade Vaz <renanandrade_rj@hotmail.com>
2013-05-16 23:39:15 -0300

    -

--- WebContent/WEB-INF/jsp/analise/analisar.jsp
+++ WebContent/WEB-INF/jsp/analise/analisar.jsp
@@ -1,4 +1,5 @@
 <br><br><br>
 Sessões tomcat: ${sessoesTomcat} <br>
+Usuários online: ${quantidadeUsuariosOnline} <br>
 Sessões abertas hibernate: ${sessoesAbertasHibernate} <br>
 Sessões fechadas hibernate: ${sessoesFechadasHibernate} <br>
--- src/scada/controller/AnaliseController.java
+++ src/scada/controller/AnaliseController.java
@@ -2,6 +2,7 @@ package scada.controller;
 
 import scada.anotacoes.Public;
 import scada.hibernate.HibernateUtil;
+import scada.interceptor.InterceptadorDeAutorizacao;
 import scada.listener.CounterListener;
 import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Resource;
@@ -23,6 +24,7 @@ public class AnaliseController {
 	public void analisar() {
 
 		result.include("sessoesTomcat", CounterListener.getCount());
+		result.include("quantidadeUsuariosOnline", InterceptadorDeAutorizacao.getUsuariosLogados().size());
 
 		HibernateUtil.gerarEstatisticas();
 
